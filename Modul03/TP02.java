4f43caa734bf7bb8040882c471554a0677e27c20
FelixNurrifiano <felixnkuliah@gmail.com>
2025-09-29 13:38:54 +0700

    TP01-03

--- Modul03/TP02.java
+++ Modul03/TP02.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class TP02 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+       
+        Scanner scanner = new Scanner(System.in);
+        
+       
+        String nama = scanner.next(); 
+        
+
+        int jumlahBarang = scanner.nextInt();
+        
+
+        int hargaPerBarang = scanner.nextInt();
+
+
+        long totalBelanja = (long)jumlahBarang * hargaPerBarang; 
+
+
+        System.out.println("Hi, " + nama + ". Total belanja adalah " + totalBelanja + " rupiah.");
+
+        scanner.close();
     }
 }
--- Modul03/TP03.java
+++ Modul03/TP03.java
@@ -1,6 +1,22 @@
+import java.util.Scanner;
+
 public class TP03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner scanner = new Scanner(System.in);
+        
+        int T = scanner.nextInt();
+        int N = scanner.nextInt();
+
+        int totalOrang = T + 1;
+
+        int jatahPermen = N / totalOrang;
+
+        int sisaPermen = N % totalOrang;
+
+        System.out.println(jatahPermen);
+        System.out.println(sisaPermen);
+
+        scanner.close();
     }
 }
