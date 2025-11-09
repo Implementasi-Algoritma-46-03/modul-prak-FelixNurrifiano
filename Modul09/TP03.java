8cb0cb328eeebff6e7c8bbc4e2d9f374ce3da34e
FelixNurrifiano <felixnkuliah@gmail.com>
2025-11-09 21:25:47 +0700

    tp09

--- Modul09/TP03.java
+++ Modul09/TP03.java
@@ -1,6 +1,20 @@
+import java.util.Scanner;
+
 public class TP03 {
 
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt();
+        long hasil = faktorial(N);
+        System.out.println(hasil);
+        input.close();
+    }
+
+    public static long faktorial(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * faktorial(n - 1);
+        }
     }
 }
