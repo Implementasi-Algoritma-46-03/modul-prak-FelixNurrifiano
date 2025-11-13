6ebeedcb87de31948ad3412790751e529ffb1b04
FelixNurrifiano <felixnkuliah@gmail.com>
2025-11-13 15:57:32 +0700

    modul09

--- Modul09/Jurnal03.java
+++ Modul09/Jurnal03.java
@@ -1,6 +1,27 @@
-public class Jurnal03 {
+import java.util.Scanner;
 
+public class Jurnal03 {
     public static void main(final String[] args) {
-        // Kerjakan soalnya di sini
+        Scanner input = new Scanner(System.in);
+        int N = input.nextInt(); 
+
+        boolean prima = true;
+
+        if (N <= 1) {
+            prima = false; 
+        } else {
+            for (int i = 2; i * i <= N; i++) {
+                if (N % i == 0) {
+                    prima = false;
+                    break;
+                }
+            }
+        }
+
+        if (prima) {
+            System.out.println("YA");
+        } else {
+            System.out.println("BUKAN");
+        }
     }
 }
--- Modul09/TP01.java
+++ Modul09/TP01.java
@@ -17,3 +17,4 @@ public class TP01 {
         input.close();
     }
 }
+
