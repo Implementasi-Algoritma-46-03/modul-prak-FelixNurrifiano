3a05a78a6cbeee43cc5235c1409d5f434e778c23
FelixNurrifiano <felixnkuliah@gmail.com>
2025-11-13 16:32:46 +0700

    modul09

--- Modul09/Jurnal03.java
+++ Modul09/Jurnal03.java
@@ -1,25 +1,28 @@
 import java.util.Scanner;
 
 public class Jurnal03 {
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         Scanner input = new Scanner(System.in);
-        int n = input.nextInt();
-        if (n <= 1) {
-            System.out.println("BUKAN");
+        int N = input.nextInt();
+        input.close();
+
+        int prima = 1; 
+
+        if (N <= 1) {
+            prima = 0; 
         } else {
-            boolean prima = true;
-            for (int i = 2; i <= Math.sqrt(n); i++) {
-                if (n % i == 0) {
-                    prima = false;
+            for (int i = 2; i * i <= N; i++) {
+                if (N % i == 0) {
+                    prima = 0; 
                     break;
                 }
             }
-            if (prima) {
-                System.out.println("YA");
-            } else {
-                System.out.println("BUKAN");
-            }
         }
-        input.close();
+
+        if (prima == 1) {
+            System.out.println("YA");
+        } else {
+            System.out.println("BUKAN");
+        }
     }
 }
