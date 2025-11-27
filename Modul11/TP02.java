78bd0d7d5412c18c7e387629ba3558a9f2f43e7f
FelixNurrifiano <felixnkuliah@gmail.com>
2025-11-28 00:02:57 +0700

    modul11

--- Modul11/TP02.java
+++ Modul11/TP02.java
@@ -1,27 +1,19 @@
-import java.util.Arrays;
-import java.util.Scanner;
+import java.util.*;
 
 public class TP02 {
-
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
         int N = sc.nextInt();
-        int[] poin = new int[N];
+        int[] arr = new int[N];
 
         for (int i = 0; i < N; i++) {
-            poin[i] = sc.nextInt();
+            arr[i] = sc.nextInt();
         }
 
-        Arrays.sort(poin);
-
+        Arrays.sort(arr);
         for (int i = N - 1; i >= 0; i--) {
-            System.out.print(poin[i]);
-            if (i > 0) {
-                System.out.print(" ");
-            }
+            System.out.print(arr[i] + " ");
         }
-
-        sc.close();
     }
 }
