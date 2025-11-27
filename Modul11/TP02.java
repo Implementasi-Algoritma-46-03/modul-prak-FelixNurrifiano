7fb6f8228194ffe412e2a39c18f626f01602405e
FelixNurrifiano <felixnkuliah@gmail.com>
2025-11-28 00:16:13 +0700

    moduls11

--- Modul11/TP02.java
+++ Modul11/TP02.java
@@ -4,17 +4,23 @@ public class TP02 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        if (!sc.hasNextInt()) return;  
         int N = sc.nextInt();
-        int[] arr = new int[N];
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
+        ArrayList<Integer> list = new ArrayList<>();
+
+        while (list.size() < N && sc.hasNextInt()) {
+            list.add(sc.nextInt());
         }
 
-        Arrays.sort(arr); // ascending
+       
+        if (list.size() < N) return;
+
+        Collections.sort(list);
 
         for (int i = N - 1; i >= 0; i--) {
-            System.out.print(arr[i] + " ");
+            System.out.print(list.get(i));
+            if (i > 0) System.out.print(" ");
         }
     }
 }
