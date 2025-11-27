6b875757afdeb4a6b8764a331dfc4147ab90abed
FelixNurrifiano <felixnkuliah@gmail.com>
2025-11-28 00:26:03 +0700

    modul11s

--- Modul11/TP02.java
+++ Modul11/TP02.java
@@ -2,20 +2,35 @@ import java.util.*;
 
 public class TP02 {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        int N = sc.nextInt();
-        ArrayList<Integer> list = new ArrayList<>();
-
+        Scanner scanner = new Scanner(System.in);
+        
+        int N = scanner.nextInt();
+        
+        ArrayList<Integer> points = new ArrayList<>();
+        
         for (int i = 0; i < N; i++) {
-            list.add(sc.nextInt());
+            int poin = scanner.nextInt();
+            points.add(poin);
         }
-
-        Collections.sort(list);
-
-        for (int i = N - 1; i >= 0; i--) {
-            System.out.print(list.get(i));
-            if (i > 0) System.out.print(" ");
+        
+        for (int i = 0; i < N - 1; i++) {
+            for (int j = 0; j < N - i - 1; j++) {
+                if (points.get(j) < points.get(j + 1)) {
+                    int temp = points.get(j);
+                    points.set(j, points.get(j + 1));
+                    points.set(j + 1, temp);
+                }
+            }
         }
+        
+        for (int i = 0; i < N; i++) {
+            if (i > 0) {
+                System.out.print(" ");
+            }
+            System.out.print(points.get(i));
+        }
+        System.out.println();
+        
+        scanner.close();
     }
 }
