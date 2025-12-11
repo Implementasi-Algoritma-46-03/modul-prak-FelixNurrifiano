ce10d995e0d44036b92203ac5ed43cbcde4f4e66
FelixNurrifiano <felixnkuliah@gmail.com>
2025-12-11 15:01:14 +0700

    test

--- Modul13/TP02.java
+++ Modul13/TP02.java
@@ -6,32 +6,29 @@ public class TP02 {
         double[] wave1 = new double[10];
         double[] wave2 = new double[10];
 
-        // Input gelombang 1
+   
         for (int i = 0; i < 10; i++) {
             wave1[i] = sc.nextDouble();
         }
 
-        // Input gelombang 2
+
         for (int i = 0; i < 10; i++) {
             wave2[i] = sc.nextDouble();
         }
 
-        // Sorting ascending dulu
         java.util.Arrays.sort(wave1);
         java.util.Arrays.sort(wave2);
 
-        // Lalu reverse agar jadi descending
         reverse(wave1);
         reverse(wave2);
 
-        // Output gelombang 1 (descending)
+
         for (int i = 0; i < 10; i++) {
             System.out.printf("%.2f", wave1[i]);
             if (i < 9) System.out.print(" ");
         }
         System.out.println();
 
-        // Output gelombang 2 (descending)
         for (int i = 0; i < 10; i++) {
             System.out.printf("%.2f", wave2[i]);
             if (i < 9) System.out.print(" ");
@@ -39,7 +36,6 @@ public class TP02 {
         System.out.println();
     }
 
-    // Fungsi untuk membalik array (reverse)
     public static void reverse(double[] arr) {
         for (int i = 0; i < arr.length / 2; i++) {
             double temp = arr[i];
