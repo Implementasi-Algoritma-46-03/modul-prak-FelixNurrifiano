ff0476e08739c0c15c7bf516a9a4bc08c6903f77
FelixNurrifiano <felixnkuliah@gmail.com>
2025-12-11 14:56:14 +0700

    tptp

--- Modul13/TP01.java
+++ Modul13/TP01.java
@@ -6,28 +6,23 @@ public class TP01 {
         double[] wave1 = new double[10];
         double[] wave2 = new double[10];
 
-        // Input gelombang 1
         for (int i = 0; i < 10; i++) {
             wave1[i] = sc.nextDouble();
         }
 
-        // Input gelombang 2
         for (int i = 0; i < 10; i++) {
             wave2[i] = sc.nextDouble();
         }
 
-        // Sorting
         java.util.Arrays.sort(wave1);
         java.util.Arrays.sort(wave2);
 
-        // Output gelombang 1
         for (int i = 0; i < 10; i++) {
             System.out.printf("%.2f", wave1[i]);
             if (i < 9) System.out.print(" ");
         }
         System.out.println();
 
-        // Output gelombang 2
         for (int i = 0; i < 10; i++) {
             System.out.printf("%.2f", wave2[i]);
             if (i < 9) System.out.print(" ");
